65f489b92d0d5037855edf854e3552ae093f576d
jesus alvarez <jesus.alvarez.093@gmail.com>
2026-07-16 11:25:06 -0400

    add: generador de saludos con calculo de anio de nacimiento

---
+++ gene-saludos.py
@@ -0,0 +1,9 @@
+usuario = input(f"ingrese su usuario ")
+edad = int(input(f"ingrese su edad "))
+
+year = 2026 
+
+nacimiento = year - edad 
+
+print(f"hola {usuario}, nacieste en el año {nacimiento}")
+
