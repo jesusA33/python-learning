737aff7c477aef92263aff953dbc56b2889a157b
jesus alvarez <jesus.alvarez.093@gmail.com>
2026-07-19 13:22:30 -0400

    correcion de calculadora de imc

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -4,4 +4,7 @@ altura = float(input("Por favor ingrese su altura "))
 
 imc = peso / (altura ** 2) 
 
-print(f"su IMC es de {imc:.2f}")
+
+print(f"Peso (kg) {peso}")
+print(f"Altura (m) {altura}")
+print(f"Tu IMC es de {imc:.2f}")
