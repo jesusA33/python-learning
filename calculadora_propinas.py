f3ac13e71d857a88568d869891b14f937a209b23
jesus alvarez <jesus.alvarez.093@gmail.com>
2026-07-19 13:21:51 -0400

    correccion de calculadora de propinas

--- calculadora_propinas.py
+++ calculadora_propinas.py
@@ -1,6 +1,6 @@
 monto = float(input("Ingrese su monto "))
 
-porcentaje = float(input("ingrse su porcentaje de propina 10, 15, 20% "))
+porcentaje = float(input("Ingrse su porcentaje de propina 10, 15, 20% "))
 
 propina = (monto * porcentaje / 100)
 
